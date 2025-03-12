008e7b0f8d88d0c56449877d4b5b3dec2e754095
gravitiwork <graviti.work@outlook.com>
2025-03-12 20:50:51 +0530

    Update Cutadapt.sh

--- Cutadapt.sh
+++ Cutadapt.sh
@@ -1,1 +1,11 @@
-### bash code 
+
+### bash code by Study Tech
+
+
+cutadapt -q 20 -o Trimmed_SRR23085866_1.fastq SRR23085866_1.fastq
+
+cutadapt -q 20 -o Trimmed_SRR23085866_2.fastq SRR23085866_2.fastq
+
+
+
+#### Run this code by $sh Cutadapt.sh and Enter
