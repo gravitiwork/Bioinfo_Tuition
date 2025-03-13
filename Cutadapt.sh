4faef70d81fefa23a84e31ca36eb02a4fe0ab5bd
gravitiwork <graviti.work@outlook.com>
2025-03-13 08:19:56 +0530

    Update Cutadapt.sh

--- Cutadapt.sh
+++ Cutadapt.sh
@@ -1,9 +1,13 @@
 
 ### bash code by Study Tech
 
+##### These are the code to unzip the fastq files
+gzip -d SRR23085866_1.fastq.gz
+gzip -d SRR23085866_2.fastq.gz
+
+##### There are the simple code for cutadapt
 
 cutadapt -q 20 -o Trimmed_SRR23085866_1.fastq SRR23085866_1.fastq
-
 cutadapt -q 20 -o Trimmed_SRR23085866_2.fastq SRR23085866_2.fastq
 
 
