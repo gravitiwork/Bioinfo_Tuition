a4fb68082528285ff997cf89e5d5e55f881420eb
gravitiwork <graviti.work@outlook.com>
2025-03-14 16:40:54 +0530

    Add files via upload

---
+++ 02_bowtie2_map.sh
@@ -0,0 +1,13 @@
+
+############# After "01_Cutadapt.sh" run use this script 
+
+###### In this script We will do mapping of reads
+
+###### Here the input files are Trimmed_SRR23085866_1.fastq, Trimmed_SRR23085866_2.fastq 
+
+###### Those 6 files (like "cat_index.1.bt2") will be used as genome index
+
+###### Output file will be SRR23085866.sam. 
+
+
+bowtie2 -x cat_index -q -1 Trimmed_SRR23085866_1.fastq -2 Trimmed_SRR23085866_2.fastq --no-unal --local --score-min C,50,1 --threads 4 -S SRR23085866.sam
